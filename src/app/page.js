8c3c42eec7c3c1502acd7393dab8b91d8c82049d
suson2024arvin <aassuson@gmail.com>
2026-04-04 13:24:34 -0500

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Carol Egg Farm System</h1>
+      <p>System is live and ready.</p>
+    </main>
+  );
+}
